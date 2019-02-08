71d1d2783005b963d6d8c68f21b4e9c1afe9a154
Aditya Anand <adityaananad@gmail.com>
2019-02-08 20:52:11 +0530

    commit

--- Project 3/app.js
+++ Project 3/app.js
@@ -39,14 +39,14 @@ $(document).ready(function() {
 
   $(window).scroll(function() {
     let position = $(this).scrollTop();
-     if(position >= 5100) {
+     if(position >= 4900) {
        $('.card-1').addClass('moveFromLeft');
        $('.card-2').addClass('moveFromBottom');
        $('.card-3').addClass('moveFromRight');
      } else {
-       $('.card-1').reamoveClass('moveFromLeft');
-       $('.card-2').reamoveClass('moveFromBottom');
-       $('.card-3').reamoveClass('moveFromRight');
+       $('.card-1').removeClass('moveFromLeft');
+       $('.card-2').removeClass('moveFromBottom');
+       $('.card-3').removeClass('moveFromRight');
      }
   }); 
 }); 
